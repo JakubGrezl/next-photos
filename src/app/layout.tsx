56cfe1a23f27b638ca35a930e4aa835fd7e65f7a
JakubGrezl <jakub.grezl@outlook.cz>
2023-12-18 08:46:02 +0100

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,8 @@
-import "~/styles/globals.css";
+import "../styles/globals.css";
 
 import { Inter } from "next/font/google";
 import { Montserrat } from "next/font/google";
 import { cookies } from "next/headers";
-
-import { TRPCReactProvider } from "~/trpc/react";
 
 import Header from "./header";
 import "../styles/background.css";
