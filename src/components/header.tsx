aaed5c5e6652fa01c50e7d160a879b45f92e707c
JakubGrezl <jakub.grezl@outlook.cz>
2024-01-16 11:27:35 +0100

    fix

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,22 +1,15 @@
 import React from "react";
 import "@/styles/navbar.css";
 import Link from "next/link";
-// import { getServerSession } from "next-auth";
-// import { options } from "@/app/api/auth/[...nextauth]/options";
 
 const Header = async () => {
-  // const session = await getServerSession(options);
-  
   return (
     <React.Fragment>
       <nav>
         <h1>Next Photos</h1>
         <ul>
-          {/* {session ? (
-            <Link href="/api/auth/signout?callbackUrl=/">Logout</Link>
-          ) : (
-            <Link href="/api/auth/signin">Login</Link>
-          )} */}
+          <Link href="/map">Map</Link>
+          <Link href="/">Home</Link>
         </ul>
       </nav>
     </React.Fragment>
