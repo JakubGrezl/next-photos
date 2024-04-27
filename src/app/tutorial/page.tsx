f0e0983850334aecf846c9f08943ad6758e6e37d
JakubGrezl <jakub.grezl@outlook.cz>
2024-04-27 08:27:57 +0200

    nejaka cast toho tutorialu, neni to vse!!

--- src/app/tutorial/page.tsx
+++ src/app/tutorial/page.tsx
@@ -2,45 +2,105 @@ export default async function Page() {
   return (
     <main className="no-nav overflow-auto">
       <article className="prose mx-auto">
+        <h1>Začátky focení</h1>
+        <p>
+          Ahoj, vítám tě na stránce <strong>Next-Photos</strong>, tato stránka
+          slouží k vytváření vlastního portfolia fotografií, také se naučíš jak
+          fotit a jak zpracovávat fotografie.
+        </p>
+        <h2>Co budu potřebovat?</h2>
+        <p>
+          Na začátek si řekneme, co potřebuješ k cestě na to se stát
+          profesionálním fotografem. Na prvním místě je samozřejmě{" "}
+          <strong>fotoaparát</strong>, ovšem toto je tak trochu lež, protože na
+          začátek ti postačí i mobilní telefon s funkcí manuálního nastavení.
+          Dále budeš potřebovat počítač s aplikací <strong>Lightroom</strong>,
+          nebo jiné alternativy této aplikace.
+        </p>
+        <hr />
+        <h1>První fotografie</h1>
+        <p>Tak už máš všechno připravené? Tudíž se na to můžeme vrhnout</p>
+        <p>
+          Foťák si zapneme a přepneme ho do tzv.{" "}
+          <strong>manuálního módu</strong>
+        </p>
         <figure>
-          <img
-            src="https://images.unsplash.com/photo-1556740758-90de374c12ad?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1000&amp;q=80"
-            alt=""
-          />
+          <img src="/tutorial/manual.jpg" alt="Manuální mód" />
           <figcaption>
-            Contrary to popular belief, Lorem Ipsum is not simply random text.
-            It has roots in a piece of classical Latin literature from 45 BC,
-            making it over 2000 years old.
+            Přepnutí do manuálního módu, zdroj:
+            https://www.tamaggo.com/2022/10/27/a-quick-guide-to-using-manual-settings-on-your-canon-camera/
           </figcaption>
         </figure>
         <p>
-          For more information about how to use the plugin and the features it
-          includes,{" "}
-          <a href="https://github.com/tailwindcss/typography/blob/master/README.md">
-            read the documentation
-          </a>
-          .
+          teď ti na hlavním displaji pravděpodobně vyskočilo hrozně moc
+          nastavení, NELEKEJ SE, vše si vysvětlíme!
         </p>
-        <hr />
-        <h2>What to expect from here on out</h2>
         <p>
-          What follows from here is just a bunch of absolute nonsense I've
-          written to dogfood the plugin itself. It includes every sensible
-          typographic element I could think of, like <strong>bold text</strong>,
-          unordered lists, ordered lists, code blocks, block quotes,{" "}
-          <em>and even italics</em>.
+          Hlavní nastavení fotoaparátu je důležité pochopit, protože ovlivňuje
+          výslednou kvalitu fotografií.
         </p>
-        <p>It's important to cover all of these use cases for a few reasons:</p>
-        <ol>
-          <li>We want everything to look good out of the box.</li>
-          <li>
-            Really just the first reason, that's the whole point of the plugin.
-          </li>
-          <li>
-            Here's a third pretend reason though a list with three items looks
-            more realistic than a list with two items.
-          </li>
-        </ol>
+        <h1>ISO, clona a rychlost uzávěrky</h1>
+        <hr />
+        <h2>ISO</h2>
+        <p>
+          ISO je citlivost snímače na světlo, čím vyšší ISO, tím více světla
+          fotoaparát zachytí, bohužel zvyšováním isa se následně zhorší kvalita
+          fotografie <strong>zrněním</strong>.
+        </p>
+        <pre className="overflow-hidden whitespace-normal">
+          Tuto funkci použijeme, pokud si chceme zanechat rychlost uzávěrky a
+          clonu na jedné hodnotě, ale fotíme v tmavém prostředí, takže fotka je
+          při tomto nastavění moc tmavá.
+        </pre>
+
+        <figure>
+          <img src="/tutorial/iso.avif" alt="ukázka ISO" />
+          <figcaption>
+            Takto vypadají fotografie s různým isem, zdroj:
+            https://photographylife.com/what-is-iso-in-photography
+          </figcaption>
+        </figure>
+        <hr />
+        <h2>Clona</h2>
+        <p>
+          Clona je otvor v objektivu, který ovlivňuje hloubku ostrosti. Nižší
+          clonové číslo (např. f/1.8) znamená větší otvor a menší hloubku
+          ostrosti, zatímco vyšší clonové číslo (např. f/16) znamená menší otvor
+          a větší hloubku ostrosti.
+        </p>
+        <p>
+          <strong>Hloubka ostrosti</strong> je vzdálenost, ve které je
+          fotografie ostrá, používá se tedy pro rozmazání pozadí
+        </p>
+
+        <figure>
+          <img src="/tutorial/aperture.jpg" alt="Ukázka clony" />
+          <figcaption>
+            Ukázka různých clonových hodnot a jejich vliv na hloubku ostrosti,
+            zdroj: https://www.pinterest.com/pin/aperture--714665034588088334/
+          </figcaption>
+        </figure>
+
+        <hr />
+        <h2>Rychlost uzávěrky</h2>
+        <p>
+          Rychlost uzávěrky je doba, po kterou je snímač fotoaparátu vystaven
+          světlu. Rychlost uzávěrky se měří v sekundách a zlomcích sekundy.
+        </p>
+        <p>
+          Rychlost uzávěrky ovlivňuje to, jak dlouho je snímač fotoaparátu
+          vystaven světlu, čím delší čas, tím více světla se dostane na snímač.
+        </p>
+        <figure>
+          <img
+            src="/tutorial/shutterspeed.png"
+            alt="Ukázka rychlosti uzávěrky"
+          />
+          <figcaption>
+            Ukázka rychlosti uzávěrky, zdroj:
+            https://www.pinterest.com/pin/examples-of-shutter-speed-in-photography--637189047252210187/
+          </figcaption>
+        </figure>
       </article>
     </main>
   );
