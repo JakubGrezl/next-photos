1814b8b8f2c49cd965b4c3ed09eb0473453ebb54
JakubGrezl <jakub.grezl@outlook.cz>
2024-05-03 11:05:24 +0200

    changes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -23,7 +23,7 @@ const Header = async () => {
             </>
           )}
           <Link href="/map">Map</Link>
-          <Link href="/tutorial">TutorialPage</Link>
+          <Link href="/tutorial">Tutorial</Link>
           <Link href="/">Home</Link>
         </ul>
       </nav>
