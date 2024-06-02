c90062aac78066215773338875460c72117ad442
Jakub.Grezl <jakub.grezl@outlook.cz>
2024-06-02 23:25:10 +0200

    Update app.tsx

--- src/app/countdown/app.tsx
+++ src/app/countdown/app.tsx
@@ -1,6 +1,9 @@
 "use client";
 
-import Countdown from "react-countdown";
+import Countdown, {
+  CountdownRenderProps,
+  CountdownRendererFn,
+} from "react-countdown";
 import { useEffect, useState } from "react";
 import Image from "next/image";
 
@@ -11,9 +14,36 @@ export default function App() {
     setCountdown("2024-06-10T08:00:00");
   }, []);
 
+  const renderer: CountdownRendererFn = ({
+    days,
+    hours,
+    minutes,
+    seconds,
+    milliseconds,
+    completed,
+  }: CountdownRenderProps) => {
+    if (completed) {
+      // Render a completed state
+      return <span>Prohrali jsme</span>;
+    } else {
+      // Render a countdown
+      return (
+        <span>
+          {days > 0 && `${days} dní`}
+          {days > 0 && hours > 0 && ", "}
+          {hours > 0 && `${hours} hodin`}
+          {(days > 0 || hours > 0) && minutes > 0 && ", "}
+          {minutes > 0 && `${minutes} minut`}
+          {(days > 0 || hours > 0 || minutes > 0) && seconds > 0 && " a "}
+          {seconds > 0 && `${seconds} sekund`}
+        </span>
+      );
+    }
+  };
+
   if (countdown)
     return (
-      <div className="bg-black flex relative justify-center items-center h-screen w-full ">
+      <div className="bg-black flex flex-row relative justify-center items-center h-screen w-full ">
         <Image
           src="/gg.png"
           layout="fill"
@@ -21,7 +51,11 @@ export default function App() {
           className="opacity-30 h-screen w-full"
         />
         <span className="text-white text-lg absolute">
-          Obhajoby jsou za: <Countdown date={countdown} />
+          Obhajoby jsou za: <Countdown date={countdown} renderer={renderer} />
+        </span>
+
+        <span className="text-sm text-white flex self-end">
+          spokojenej tomáši??
         </span>
       </div>
     );
